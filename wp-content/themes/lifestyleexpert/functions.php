c0c7c961eacfa8f71e74c431328cfb14a2734375
tuananh3988 <tuananh8893@yahoo.com>
2013-12-17 02:57:05 +0700

    add new

---
+++ wp-content/themes/lifestyleexpert/functions.php
@@ -0,0 +1,5 @@
+<?php	
+register_nav_menus( array(
+  'primary'   => __( 'Top primary menu', 'lifestyleexpert' ),
+  'secondary' => __( 'Secondary menu in left sidebar', 'twentyfourteen' ),
+) );
---
+++ wp-content/themes/lifestyleexpert/header.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * The Header template for our theme
+ *
+ * Displays all of the <head> section and everything up till <div id="main">
+ *
+ * @package WordPress
+ * @subpackage Twenty_Thirteen
+ * @since Twenty Thirteen 1.0
+ */
+?><!DOCTYPE html>
+<!--[if IE 7]>
+<html class="ie ie7" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 8]>
+<html class="ie ie8" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if !(IE 7) | !(IE 8)  ]><!-->
+<html <?php language_attributes(); ?>>
+<!--<![endif]-->
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width">
+	<title><?php wp_title( '|', true, 'right' ); ?></title>
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+  <link href='http://fonts.googleapis.com/css?family=Lato:700' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo('template_directory'); ?>/style.css" />
+	<!--[if lt IE 9]>
+	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js"></script>
+	<![endif]-->
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+	<div id="page" class="hfeed site">
+		<header id="masthead" class="site-header" role="banner">
+      <div id="wrap-headder">
+        <div class="wrap-logo">
+          <a class="home-link" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+            <img alt="logo" class="logo" src="<?php bloginfo('template_directory'); ?>/images/logopage.png" />
+          </a>
+        </div>
+        <div class="main-nav"><?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?></div>
+        <div class="clear"></div>
+      </div>
+			
+
+		</header><!-- #masthead -->
+
+		<div id="main" class="site-main">
